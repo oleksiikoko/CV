e43d978d8e2e6040b5b117088f814a2f3d8eea44
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-07-16 14:46:20 +0300

    Education tests

---
+++ src/components/Education/Education.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
+
+import { Education } from "components";
+import { act } from "react-dom/test-utils";
+
+afterEach(cleanup);
+
+describe("BottomPageSwitcher", () => {
+  const items = [
+    {
+      text: "Unit Factory pool",
+      description:
+        "Was engaged in an intensive boot camp of C language course where everyone learns himself to work with strict deadlines and self-planning. Basics of networking. Introduction to Unix",
+    },
+    {
+      text: 'NTUU "Igor Sikorsky Kyiv Polytechnic Institute"',
+    },
+  ];
+
+  beforeEach(() => {
+    render(<Education items={items} />);
+  });
+
+  it("items have class", () => {
+    screen.getAllByTestId("education-item").map((item) => {
+      expect(item).toHaveClass("education-item");
+    });
+  });
+
+  it("DescriptionBox count", () => {
+    expect(screen.queryAllByTestId("description")).toHaveLength(
+      items.length * 2 - 1
+    );
+  });
+});
--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -6,19 +6,30 @@ import { Description } from "components";
 import "./Education.scss";
 
 const Education = ({ items }) => {
-  return items.map((item, index) => {
-    return (
-      <div className="education-item" key={index}>
-        <Description description={item.text} className="education-item__name" />
-        {item.description && (
-          <Description
-            description={item.description}
-            className="education-item__description"
-          />
-        )}
-      </div>
-    );
-  });
+  return (
+    <>
+      {items.map((item, index) => {
+        return (
+          <div
+            className="education-item"
+            key={index}
+            data-testid="education-item"
+          >
+            <Description
+              description={item.text}
+              className="education-item__name"
+            />
+            {item.description && (
+              <Description
+                description={item.description}
+                className="education-item__description"
+              />
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 Education.propTypes = {
