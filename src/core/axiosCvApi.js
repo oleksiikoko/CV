449b5c8ef3271bf8849fadc2dbb670f05f5b6de0
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-07-21 17:51:44 +0300

    Gitpages

--- src/core/axiosCvApi.js
+++ src/core/axiosCvApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 //json-server
 // axios.defaults.baseURL = "http://localhost:3002";
-axios.defaults.baseURL = "http://192.168.1.12:3002";
+// axios.defaults.baseURL = "http://192.168.1.12:3002";
+axios.defaults.baseURL = "https://kovalchuk-cv.herokuapp.com";
 
 export default axios;
